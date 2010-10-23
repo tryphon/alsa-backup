8bb3ccc3eda7437a3dd56775964013babb940c68
Alban Peignier <alban@tryphon.eu>
2010-10-23 17:17:49 +0200

    Use lets and subject in Writer specs

--- spec/alsa_backup/writer_spec.rb
+++ spec/alsa_backup/writer_spec.rb
@@ -2,20 +2,19 @@ require File.dirname(__FILE__) + '/../spec_helper.rb'
 
 describe AlsaBackup::Writer do
 
-  before(:each) do
-    @file = "test.wav"
-    @directory = test_directory
-    @writer = AlsaBackup::Writer.new :directory => @directory, :file => @file
-  end
+  let(:file) { "test.wav" }
+  let(:directory) { test_directory }
+
+  subject { AlsaBackup::Writer.new :directory => directory, :file => file }
 
   context "when created" do
 
     it "should use the :directory option as directory" do
-      AlsaBackup::Writer.new(:directory => @directory).directory.should == @directory
+      AlsaBackup::Writer.new(:directory => directory).directory.should == directory
     end
 
     it "should use the :file option as file" do
-      AlsaBackup::Writer.new(:file => @file).file.should == @file
+      AlsaBackup::Writer.new(:file => file).file.should == file
     end
 
     it "should use the :format option as format" do
@@ -53,12 +52,12 @@ describe AlsaBackup::Writer do
     let(:callback) { TestCallback.new }
 
     before(:each) do
-      @writer.on_close_callbacks << callback
+      subject.on_close_callbacks << callback
     end
     
     it "should check if file is empty" do
-      AlsaBackup::Writer.should_receive(:delete_empty_file).with(@file).and_return(true)
-      @writer.on_close(@file)
+      AlsaBackup::Writer.should_receive(:delete_empty_file).with(file).and_return(true)
+      subject.on_close(file)
     end
 
     context "when file was empty" do
@@ -68,20 +67,20 @@ describe AlsaBackup::Writer do
       end
 
       it "should not invoke callbacks" do
-        @writer.on_close(@file)
+        subject.on_close(file)
         callback.should_not be_invoked
       end
 
     end
 
     it "should invoke all on_close_callbacks" do
-      @writer.on_close(@file)
+      subject.on_close(file)
       callback.should be_invoked
     end
 
     it "should ignore exception from callbacks" do
       callback.stub!(:call).and_raise("Error")
-      lambda { @writer.on_close(@file) }.should_not raise_error
+      lambda { subject.on_close(file) }.should_not raise_error
     end
 
   end
@@ -134,14 +133,14 @@ describe AlsaBackup::Writer do
   describe "file" do
     
     it "should accept file as string" do
-      @writer.file = file_name = "dummy"
-      @writer.file.should == file_name
+      subject.file = file_name = "dummy"
+      subject.file.should == file_name
     end
 
     it "should accept file as Proc" do
       file_name = "dummy"
-      @writer.file = Proc.new { file_name }
-      @writer.file.should == file_name
+      subject.file = Proc.new { file_name }
+      subject.file.should == file_name
     end
     
   end
@@ -149,25 +148,25 @@ describe AlsaBackup::Writer do
   describe "rename_existing_file" do
 
     it "should keep file if not exists" do
-      File.should_receive(:exists?).with(@file).and_return(false)
+      File.should_receive(:exists?).with(file).and_return(false)
       File.should_not_receive(:rename)
-      AlsaBackup::Writer.rename_existing_file(@file)
+      AlsaBackup::Writer.rename_existing_file(file)
     end
 
     it "should try to suffix with '-n' to find a free name" do
       File.stub!(:exists?).and_return(true)
 
-      free_file = File.suffix_basename(@file, "-99")
+      free_file = File.suffix_basename(file, "-99")
       File.should_receive(:exists?).with(free_file).and_return(false)
 
-      File.should_receive(:rename).with(@file, free_file)
-      AlsaBackup::Writer.rename_existing_file(@file)
+      File.should_receive(:rename).with(file, free_file)
+      AlsaBackup::Writer.rename_existing_file(file)
     end
 
     it "should raise an error when no free file is found" do
       File.stub!(:exists?).and_return(true)
       lambda do
-        AlsaBackup::Writer.rename_existing_file(@file)
+        AlsaBackup::Writer.rename_existing_file(file)
       end.should raise_error
     end
 
@@ -176,8 +175,8 @@ describe AlsaBackup::Writer do
   describe "close" do
 
     it "should close file (via close_file method)" do
-      @writer.should_receive(:close_file)
-      @writer.close
+      subject.should_receive(:close_file)
+      subject.close
     end
     
   end
@@ -185,14 +184,14 @@ describe AlsaBackup::Writer do
   describe "close_file" do
     
     it "should close current sndfile" do
-      sndfile = @writer.sndfile
+      sndfile = subject.sndfile
       sndfile.should_receive(:close)
-      @writer.close_file
+      subject.close_file
     end
 
     it "should notify on_close callbacks (via on_close method)" do
-      @writer.should_receive(:on_close).with(@writer.sndfile.path)
-      @writer.close_file
+      subject.should_receive(:on_close).with(subject.sndfile.path)
+      subject.close_file
     end
 
   end
